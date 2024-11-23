12417c38d6301ca13a8f831dff4c9751fbbdfca4
Sioxox <21986104+regisandre@users.noreply.github.com>
2024-11-23 18:46:37 +0100

    Landing page without videos

---
+++ assets/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-e0b5496619247ba2.js","static/chunks/pages/index-cc442d292595c101.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
