ea82f98baf03d09e0a5a7467e22cc0ffbf75668c
regisandre <21986104+regisandre@users.noreply.github.com>
2025-06-30 23:35:53 +0200

    feat(app): add Google Analytics gtag.js integration for tracking

--- app.vue
+++ app.vue
@@ -14,6 +14,25 @@
 </template>
 
 <script setup>
+import { useHead } from '#imports'
+
+// Google Analytics gtag.js global integration
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-3PPVHPNSL5',
+      async: true
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-3PPVHPNSL5');
+      `
+    }
+  ]
+})
 useHead({
   htmlAttrs: {
     'data-theme': 'light'
